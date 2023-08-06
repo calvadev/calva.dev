7ae46ae3488b3fa6a7193c48c432581b6eb0d218
calvadev <32919103+calvadev@users.noreply.github.com>
2023-08-06 23:38:56 +0000

    Removed NIP-05 identification (didn't function properly)

--- src/next.config.js
+++ src/next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-        return [
-            {
-                source: "/.well-known/nostr.json",
-                headers: [
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                ]
-            }
-        ]
-    },
 }
 
 module.exports = nextConfig
