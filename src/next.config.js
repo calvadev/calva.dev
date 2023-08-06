a26df37097169526d42dd255fe1f7de61cfaa270
calvadev <32919103+calvadev@users.noreply.github.com>
2023-08-06 21:47:35 +0000

    Set up CORS permissions

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/.well-known/nostr.json",
+                headers: [
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                ]
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
