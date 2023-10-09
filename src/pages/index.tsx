fbfc928014e4c39bd045ac5fd41a0cd2cdedc845
calvadev <32919103+calvadev@users.noreply.github.com>
2023-10-09 23:57:51 +0000

    Fixed text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,13 +10,13 @@ export default function About() {
         I am currently working as a software engineer in Seattle, WA, U.S.A. and have a deep interest in freedom tech, particulary Bitcoin and Nostr.
       </p>
       <p className="py-1 px-3 mb-3">
-        I believe that Bitcoin has the potential to open up previously ignored or irrelevant countries to the rest of the world in regards to financial access and tapping into their human capital. I think is the most open and important free market monetary system that has existed in the history of humanity.
+        I believe that Bitcoin has the potential to open up previously ignored or irrelevant countries to the rest of the world in regards to financial access and tapping into their human capital. I think it is the most open and important free market monetary system that has existed in the history of humanity.
       </p>
       <p className="py-1 px-3 mb-3">
         Nostr, on the other hand, facilitates the adoption and spread of Bitcoin while also allowing for the construction of globally distributed, decentralized, and interoperable applications, whether that be social media platforms, marketplaces, or anything else you can think of that can handle JSON objects.
       </p>
       <p className="py-1 px-3 mb-3">
-        Outside of full-time work, I have been focusing on building a Nostr marketplace client called Shopstr and on contributing to Cashu, which is a Chaumain ecash system built for Bitcoin.
+        Outside of full-time work, I have been focused on building a Nostr marketplace client called Shopstr and on contributing to Cashu, which is a Chaumain ecash system built for Bitcoin.
       </p>
       <div className="w-1/2 mx-auto my-4">
         <img alt="" src="/calvadevbtc.jpg" />
