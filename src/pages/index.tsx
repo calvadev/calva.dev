39f57186de6ab86828ee1a801e0f2f1e12f0da1d
calvadev⚡️ <32919103+calvadev@users.noreply.github.com>
2025-01-24 04:12:12 +0000

    Updated about content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function About() {
     <div className="w-full h-full p-3">
       <p className="py-1 px-3 mb-3">Hi 👋, I&apos;m calvadev!</p>
       <p className="py-1 px-3 mb-3">
-        I am the CEO and co-founder of{" "}
+        I am the CEO and founder of{" "}
         <Link href="https://shopstr.store/">Shopstr</Link>, a Nostr marketplace
         client focused on facilitating anonymous, p2p transactions using Bitcoin
         and ecash.
